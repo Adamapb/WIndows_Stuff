71caf1a7ee59871f6293c96ce5588bdd32d81cd3
Adamapb <89477210+Adamapb@users.noreply.github.com>
2021-12-10 13:29:39 -0500

    Update winscript2.bat

--- winscript2.bat
+++ winscript2.bat
@@ -1,17 +1,18 @@
 #**EXPERIMENTAL** MENU FUNCTION BELOW
 @echo off
-echo Checking if script contains Administrative rights...
+echo Checking for admin privs...
 net sessions
 if %errorlevel%==0 (
-echo Success!
+echo We good.
 ) else (
-echo No admin, please run with Administrative rights...
+echo Rerun with admin privs
 pause
 exit
 )
+#Rootkit scan needs to be reviewed
 :MENU
 echo Choose An option:
-echo 1. Hacktools
+echo 1. Hacking tools removal
 echo 2. System Scan
 echo 3. Rootkit Scan
 
@@ -21,48 +22,72 @@ if ERRORLEVEL 2 goto Two
 if ERRORLEVEL 1 goto One
 
 
-#ONE
+#Hacktools removal
 :One
 echo Finding Hacktools now...
 findstr "Cain" programfiles.flashed
 if %errorlevel%==0 (
-echo Cain detected. Please take note, then press any key.
+echo Cain detected. WRITE DOWN, ask Adam if help needed
 pause >NUL
 )
 cls
 findstr "nmap" programfiles.flashed
 if %errorlevel%==0 (
-echo Nmap detected. Please take note, then press any key.
+echo Nmap detected. WRITE DOWN, ask Adam if help needed
 pause >NUL
 )
 cls
 findstr "keylogger" programfiles.flashed
 if %errorlevel%==0 (
-echo Potential keylogger detected. Please take note, then press any key.
+echo Potential keylogger detected. WRITE DOWN, ask Adam if help needed
 pause >NUL
 )
 cls
 findstr "Armitage" programfiles.flashed
 if %errorlevel%==0 (
-echo Potential Armitage detected. Please take note, then press any key.
+echo Potential Armitage detected. WRITE DOWN, ask Adam if help needed
 pause >NUL
 )
 cls
 findstr "Metasploit" programfiles.flashed
 if %errorlevel%==0 (
-echo Potential Metasploit framework detected. Please take note, then press any key.
+echo Potential Metasploit framework detected. WRITE DOWN, ask Adam if help needed
 pause >NUL
 )
 cls
 findstr "Shellter" programfiles.flashed
 if %errorlevel%==0 (
-echo Potential Shellter detected. Please take note, then press any key.
+echo Potential Shellter detected. WRITE DOWN, ask Adam if help needed
+pause >NUL
+)
+cls
+findstr "narc" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Shellter detected. WRITE DOWN, ask Adam if help needed
+pause >NUL
+)
+cls
+findstr "malware" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Shellter detected. WRITE DOWN, ask Adam if help needed
+pause >NUL
+)
+cls
+findstr "rtkit" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Shellter detected. WRITE DOWN, ask Adam if help needed
+pause >NUL
+)
+cls
+findstr "rootkit" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Shellter detected. WRITE DOWN, ask Adam if help needed
 pause >NUL
 )
 cls
 goto MENU
 
-#TWO
+#System Scan
 :Two
 REM START SYS INTEG SCAN!
 echo "STARTING SYSTEM INTERGRITY SCAN"
@@ -70,7 +95,7 @@ echo "If it fails make sure you can access Sfc.exe"
 Sfc.exe /scannow
 goto MENU
 
-#THREE
+#Rootkit Scan
 :Three
 REM PowerShell RootKit detection start
 echo "POWERSHELL ROOTKIT DETECTION WITH MALWAREBYTES ROOTKIT BETA (Requires powershell execution policy)"
